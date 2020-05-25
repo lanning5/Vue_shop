f2856675ee5c53c9d3bbc4acc3598a5cdc2f142c
linser <1269718996@qq.com>
2020-05-25 21:50:07 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 // 导入element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 // 导入axios
-import axios from 'axios'
+
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
